0ed72a5fdea977bbda06e8bf2fa04c23525060ed
Harshit <harshitsingharya24@gmail.com>
2025-03-16 16:10:27 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
     try {
       setTimeout(async () => {
-        const response = await fetch("http://localhost:5000/chat", {
+        const response = await fetch("https://college-project-8cxh.onrender.com/chat", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
